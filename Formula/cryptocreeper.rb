35b77199aeddc480c5c6386bd59beff9ae6d2569
CodingAxolotl <superkalikimaka@gmail.com>
2026-01-11 17:37:26 +0000

    Update CryptoCreeper version to 2.0

--- Formula/cryptocreeper.rb
+++ Formula/cryptocreeper.rb
@@ -3,7 +3,7 @@ class Cryptocreeper < Formula
   homepage "https://github.com/codingaxolotl/homebrew-cryptocreeper"
   url "https://raw.githubusercontent.com/codingaxolotl/homebrew-cryptocreeper/main/cryptocreeper.png"
   sha256 "efcf861efb9c791ae58265614af9ea7d9f140f00d9b2a6ef0b42b483c08271cf"
-  version "1.0"
+  version "2.0"
 
   def install
     # Store in pkgshare first (safe)
