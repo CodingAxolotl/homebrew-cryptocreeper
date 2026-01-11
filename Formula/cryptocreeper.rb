741b33b4aed4f9c32cab0ece7f9344ef441a81c8
CodingAxolotl <superkalikimaka@gmail.com>
2026-01-11 17:13:26 +0000

    Add files via upload

---
+++ Formula/cryptocreeper.rb
@@ -0,0 +1,22 @@
+class Cryptocreeper < Formula
+  desc "Installs a legendary CryptoCreeper image"
+  homepage "https://github.com/codingaxolotl/homebrew-cryptocreeper"
+  url "https://raw.githubusercontent.com/codingaxolotl/homebrew-cryptocreeper/main/cryptocreeper.png"
+  sha256 "efcf861efb9c791ae58265614af9ea7d9f140f00d9b2a6ef0b42b483c08271cf  cryptocreeper.png"
+  version "1.0"
+
+  def install
+    pkgshare.install "cryptocreeper.png"
+  end
+
+  def caveats
+    <<~EOS
+      🧨 CryptoCreeper installed!
+
+      Image location:
+        #{pkgshare}/cryptocreeper.png
+
+      Copy it wherever you want 😄
+    EOS
+  end
+end
