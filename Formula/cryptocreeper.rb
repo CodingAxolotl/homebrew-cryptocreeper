24dad9f5e94b8b19e6d4ffe823c25b84122c1432
CodingAxolotl <superkalikimaka@gmail.com>
2026-01-11 17:47:13 +0000

    Update CryptoCreeper formula version and install process

--- Formula/cryptocreeper.rb
+++ Formula/cryptocreeper.rb
@@ -3,29 +3,35 @@ class Cryptocreeper < Formula
   homepage "https://github.com/codingaxolotl/homebrew-cryptocreeper"
   url "https://raw.githubusercontent.com/codingaxolotl/homebrew-cryptocreeper/main/cryptocreeper.png"
   sha256 "efcf861efb9c791ae58265614af9ea7d9f140f00d9b2a6ef0b42b483c08271cf"
-  version "2.0"
+  version "1.1" # bump this if you update formula
 
   def install
-    # Store in pkgshare first (safe)
-    pkgshare.install "cryptocreeper.png"
-    
-    # Also copy to user's Downloads folder
+    require "fileutils"
+
+    # Make sure Downloads folder exists
     downloads = File.join(Dir.home, "Downloads")
-    FileUtils.mkdir_p(downloads) # create Downloads folder if it somehow doesn't exist
-    FileUtils.cp("#{pkgshare}/cryptocreeper.png", downloads)
+    FileUtils.mkdir_p(downloads)
+
+    # Copy the downloaded image directly from build directory to Downloads
+    FileUtils.cp("cryptocreeper.png", downloads)
+
+    # Also install to Homebrew pkgshare (optional, safe)
+    pkgshare.install "cryptocreeper.png"
+
+    # Fun install message
+    ohai "🧨 CryptoCreeper has landed in your Downloads!"
+    ohai "You can also find it in Homebrew pkgshare: #{pkgshare}/cryptocreeper.png"
   end
 
   def caveats
     <<~EOS
-      🧨 CryptoCreeper installed!
+      🎉 CryptoCreeper installed successfully!
 
       Image location in Downloads:
         ~/Downloads/cryptocreeper.png
 
-      Or in Homebrew pkgshare:
+      Homebrew-safe copy:
         #{pkgshare}/cryptocreeper.png
-
-      Enjoy 😄
     EOS
   end
 end
