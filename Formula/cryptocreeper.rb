995ff0f37ed33330792acd60fef99a21df6e65c4
CodingAxolotl <superkalikimaka@gmail.com>
2026-01-11 17:16:43 +0000

    Fix sha256 line formatting in cryptocreeper.rb

--- Formula/cryptocreeper.rb
+++ Formula/cryptocreeper.rb
@@ -2,7 +2,7 @@ class Cryptocreeper < Formula
   desc "Installs a legendary CryptoCreeper image"
   homepage "https://github.com/codingaxolotl/homebrew-cryptocreeper"
   url "https://raw.githubusercontent.com/codingaxolotl/homebrew-cryptocreeper/main/cryptocreeper.png"
-  sha256 "efcf861efb9c791ae58265614af9ea7d9f140f00d9b2a6ef0b42b483c08271cf  cryptocreeper.png"
+  sha256 "efcf861efb9c791ae58265614af9ea7d9f140f00d9b2a6ef0b42b483c08271cf"
   version "1.0"
 
   def install
